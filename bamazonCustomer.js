e00ca136fe0a899cf25826435c2dbc0b13e3b7d3
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-29 15:52:47 -0700

    took out Connenctionend()

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,13 +17,6 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  afterConnection();
+  
 });
 
-function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-    connection.end();
-  });
-}
