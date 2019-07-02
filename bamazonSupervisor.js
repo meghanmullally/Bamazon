9844d31096482aa8df098ef0d8c7653c1ebb11f4
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-01 19:22:06 -0700

    newDep connection.query deptInput created

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -24,38 +24,75 @@ connection.connect(function (err) {
 
 function startSuper() {
 
-inquirer.prompt([
-{
-  name: "viewProduct",
-  type: "list",
-  choices: [
-    "View Product Sales bye Department",
-    "Create New Department"
-  ]
-}
-]).then(function(response) {
+  inquirer.prompt([{
+    name: "viewProduct",
+    type: "list",
+    choices: [
+      "View Product Sales by Department",
+      "Create New Department"
+    ]
+  }]).then(function (response) {
 
-switch (key) {
-  case value:
+    switch (response.viewProduct) {
+      case "View Product Sales by Department":
+        salesByDep();
+        break;
+      case "Create New Department":
+        newDep();
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  // VIEW PRODUCT SALES BY DEP
+  function salesByDep() {
+    console.log("loading product sales...");
+
+  }
+
+
+  // CREATE NEW DEP 
+  function newDep() {
+    // console.log("loading new department...");
+    inquirer.prompt([{
+        name: "newDepName",
+        type: "input",
+        message: "What is the name of the new Department?"
+      },
+      {
+        name: "overHead",
+        type: 'input',
+        message: "How much overhead?"
+      }
+
+    ]).then(function (answer) {
+
+      var depInput = [
+        [
+
+          answer.newDepName,
+          answer.overHead
+
+
+        ]
+      ]
+
+      var queryDep = "INSERT INTO departments  (department_name, over_head_costs) VALUES ?";
+      connection.query(queryDep, [depInput], function (err) {
+        if (err) throw err;
+
+        // console.log(data);
+      })
+      salesByDep();
+
+    })
     
-    break;
-
-  default:
-    break;
-}
+// end } for newDep()
+  };
 
 
 
-
-
-})
-
-
-}
-
-// VIEW PRODUCT SALES BY DEP
-
-
-
-
-// CREATE NEW DEP 
+    // function startSuper end }
+  }
